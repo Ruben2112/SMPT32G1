2f0cc24f3a4d3a407f8d889e18e2bd9d39c61f50
Fhict <fhict@ip-145-93-112-147.workstations.fontys.nl>
2015-06-26 13:14:03 +0200

    Pickerview laden met POIs

--- SchoolTrip/AfspraakMakenViewController.swift
+++ SchoolTrip/AfspraakMakenViewController.swift
@@ -9,11 +9,18 @@
 import Foundation
 import UIKit
 
-class AfspraakMakenViewController : UIViewController, UIPickerViewDelegate{
+class AfspraakMakenViewController : UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
 
+    
+    @IBOutlet weak var POIpicker: UIPickerView!
+    var POIs : [POI] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         println("Afspraak maken controller")
+        getPOI()
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -29,17 +36,76 @@ class AfspraakMakenViewController : UIViewController, UIPickerViewDelegate{
     // returns the # of rows in each component..
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
     {
-        return 3
+        return POIs.count
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
-        return "Attractie - Sprookjesbos"
+        var soort : String
+        
+        switch ("1"){
+        case POIs[row].attractie:
+            soort = "Attractie - "
+        case POIs[row].sprookjesbos:
+            soort = "Sprookjesbos - "
+        case POIs[row].winkel:
+            soort = "Winkel - "
+        case POIs[row].entertainment:
+            soort = "Entertainment - "
+        case POIs[row].algemeen:
+            soort = "Algemeen - "
+        default:
+            soort = ""
+        }
+        
+        return (soort + POIs[row].naam)
     }
+
     
     @IBAction func btnSave(sender: AnyObject) {
         
         navigationController?.popToRootViewControllerAnimated(true)
         
     }
+    func getPOI(){
+        var url = Constants.getIpAdress() + Constants.getPOIUrl()
+        
+        request(.GET, url, parameters: nil)
+            .responseJSON { (request, response, json, error) in
+                if(error != nil) {
+                    NSLog("Error: \(error)")
+                    println(request)
+                    println(response)
+                }
+                else {
+                    NSLog("Success: \(url)")
+                    var json = JSON(json!)
+                    //If json is .Dictionary
+                    for (key: String, subJson: JSON) in json {
+                        
+                        var id = subJson["id"].string
+                        var naam = subJson["naam"].string
+                        var afbeelding = subJson["afbeelding"].string
+                        var latitude = subJson["latitude"].string
+                        var longitude = subJson["longitude"].string
+                        var wachttijd = subJson["wachttijd"].string
+                        var waardering = subJson["waardering"].string
+                        var attractie = subJson["sprookjesbos"].string
+                        var sprookjesbos =  subJson["sprookjesbos"].string
+                        var winkel = subJson["winkel"].string
+                        var entertainment = subJson["entertainment"].string
+                        var algemeen = subJson["algemeen"].string
+                        
+                        var poi  = POI(id: id!, naam: naam!, afbeelding: afbeelding!, latitude: latitude!, longitude: longitude!, wachttijd: wachttijd!, waardering: waardering!, attractie: attractie!, sprookjesbos: attractie!, winkel: winkel!, entertainment: entertainment!, algemeen: algemeen!)
+                        self.POIs.append(poi)
+                        
+                    }
+                    
+                    self.POIpicker.dataSource = self
+                    self.POIpicker.reloadAllComponents()
+                }
+        }
+        
+    }
+
     
 }
